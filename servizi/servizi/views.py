dd304d1a8e5ce9b707c5852521c7f048e388bc74
ldfa <luciano@defalcoalfano.it>
2017-01-05 00:52:11 +0100

    spese: + COMMIT_ID in settings.py, with spese.context_processors.py        added in settings.py template options

--- servizi/servizi/views.py
+++ servizi/servizi/views.py
@@ -3,9 +3,10 @@
 from django.shortcuts import render
 #from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from .last_commit_id import commit_id
 
 @login_required(login_url="login/")
 def home(request):
     # return HttpResponse("Hello, world. You're at the servizi index.")
     user = request.user
-    return render(request, 'home.html', { 'user': user} )
+    return render(request, 'home.html', { 'user': user, 'commit_id': commit_id} )
